dc9385feadeed3052b127ffbc84e11f62e84056a
Ben Vernazza <ben@nazzer.dev>
2026-08-09 15:36:43 +0100

    Turnstile - Switch to explicit rendering to fix intermittent token race
    Implicit rendering could invoke the data-callback before it was defined,
    depending on async script load timing, leaving turnstileToken null

--- resources/views/vendor/cloudflare-turnstile/components/scripts.blade.php
+++ resources/views/vendor/cloudflare-turnstile/components/scripts.blade.php
@@ -1,1 +1,7 @@
-<script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer {{ $attributes }}></script>
+<script src="https://challenges.cloudflare.com/turnstile/v0/api.js?onload=__turnstileOnLoad" async defer {{ $attributes }}></script>
+<script>
+    window.__turnstileOnLoad = function () {
+        (window.__turnstileQueue || []).forEach((fn) => fn());
+        window.__turnstileQueue = [];
+    };
+</script>
--- resources/views/vendor/cloudflare-turnstile/components/turnstile.blade.php
+++ resources/views/vendor/cloudflare-turnstile/components/turnstile.blade.php
@@ -12,35 +12,52 @@ $model = $attributes->has('wire:model') ? $attributes->get('wire:model') : null;
 @endphp
 
 <div
-    data-sitekey="{{ config('services.turnstile.key') }}"
+    id="{{ $id }}"
     @if ($model)
         wire:ignore
-        data-callback="{{ $id }}Callback"
-        data-expired-callback="{{ $id }}ExpiredCallback"
-        data-timeout-callback="{{ $id }}ExpiredCallback"
-        {{ $attributes->filter(fn($value, $key) => ! in_array($key, ['data-callback', 'data-expired-callback', 'data-timeout-callback', 'wire:model', 'id']))->class(['cf-turnstile']) }}
+        {{ $attributes->filter(fn($value, $key) => ! in_array($key, ['wire:model', 'id']))->class([]) }}
     @else
+        data-sitekey="{{ config('services.turnstile.key') }}"
         {{ $attributes->class(['cf-turnstile']) }}
     @endif
 ></div>
 
 @if ($model)
     <script @if($nonce) nonce="{{ $nonce }}" @endif>
-        document.addEventListener('livewire:navigated', () => {
-            window.{{ $id }}Callback = function (token) {
-                @this.set("{{ $model }}", token);
-            }
+        (function () {
+            let widgetId = null;
 
-            window.{{ $id }}ExpiredCallback = function () {
-                window.turnstile.reset();
-            }
-
-            @this.watch("{{ $model }}", (value, old) => {
-                // If there was a value, and now there isn't, reset the Turnstile.
-                if (!!old && !value) {
-                    window.turnstile.reset();
+            function render() {
+                if (widgetId !== null) {
+                    window.turnstile.remove(widgetId);
                 }
-            })
-        });
+
+                widgetId = window.turnstile.render("#{{ $id }}", {
+                    sitekey: @js(config('services.turnstile.key')),
+                    callback: (token) => @this.set("{{ $model }}", token),
+                    'expired-callback': () => window.turnstile.reset(widgetId),
+                    'timeout-callback': () => window.turnstile.reset(widgetId),
+                });
+            }
+
+            function renderWhenReady() {
+                if (window.turnstile) {
+                    render();
+                } else {
+                    (window.__turnstileQueue ??= []).push(render);
+                }
+            }
+
+            document.addEventListener('livewire:navigated', () => {
+                renderWhenReady();
+
+                @this.watch("{{ $model }}", (value, old) => {
+                    // If there was a value, and now there isn't, reset the Turnstile.
+                    if (!!old && !value && widgetId !== null) {
+                        window.turnstile.reset(widgetId);
+                    }
+                });
+            });
+        })();
     </script>
 @endif
